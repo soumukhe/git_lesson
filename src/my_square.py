f246d3473e3b06b61ac8840e3e362a917b5e302a
Soumitra Mukherji <soumukhe@cisco.com>
2017-03-06 23:29:33 -0600

    saved in my_branch with modification

--- src/my_square.py
+++ src/my_square.py
@@ -2,4 +2,9 @@ def my_square(y):
 	""" takes a value and returns the square value
 	uses the ** operator... """
 	return (y **2)
+#
+def my_square2(x):
+	return (x * x)
+
 print my_square(42)
+print my_square2(42)
